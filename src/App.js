0019e4fc4c87300ec96603d22f887b7ffad14e10
belmoreaaron33 <belmoreaaron9@gmail.com>
2024-06-21 17:51:49 -0400

    changed title

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Route, Routes } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../css/footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>
+        <h1>Aaron Belmore | Full Stack Developer</h1>
+        <address>New York, NY</address>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
