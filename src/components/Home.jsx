f7ae9c7e0c79cdbd298305cd78e40e3a1476bdbe
belmoreaaron33 <belmoreaaron9@gmail.com>
2024-06-21 15:07:41 -0400

    adding data to the project section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import resume from "../assets/Aaron Belmore Resume (1)-1-1.png";
 import image from "../assets/headshot.png";
 import project1 from "../assets/Screenshot 2024-06-21 135923.png";
+import project2 from "../assets/Screenshot 2024-06-21 144312.png";
+import project3 from "../assets/Screenshot 2024-06-21 150145.png";
 
 import "../css/home.css";
 const Home = () => {
@@ -52,48 +54,187 @@ const Home = () => {
       <div className="projects-container">
         <h3>Projects</h3>
         <div className="projects">
-          <div className="project"></div>
-          <h4>Web Sprint Challenge: Build a Web API</h4>
-          <p>
-            In this project, I developed a CRUD API for managing projects and
-            actions using Node.js and Express. Key tasks included:
-          </p>
-          <a
-            href="https://github.com/abelmore33/web-sprint-challenge-build-a-web-api"
-            target="_blank"
-          >
-            <img className="project-img" src={project1} alt="" />
-          </a>
-          <ul>
-            <li>
-              Setting up and configuring the Node.js environment with NPM
-              scripts.
-            </li>
-            <li>Creating RESTful endpoints for projects and actions.</li>
-            <li>
-              mplementing middleware for logging, validation, and error handling
-            </li>
-            <li>
-              Designing database schemas and using SQLite for data persistence.
-            </li>
-            <li>
-              Writing automated tests to ensure API functionality and
-              reliability.
-            </li>
-          </ul>
-          <p>This project showcases my skills in </p>
-        </div>
-        <div className="project">
-          <h4>Project 2</h4>
-          <p>Description</p>
-        </div>
-        <div className="project">
-          <h4>Project 3</h4>
-          <p>Description</p>
-        </div>
-        <div className="project">
-          <h4>Project 4</h4>
-          <p>Description</p>
+          <div className="project">
+            <h4>Web Sprint Challenge: Build a Web API</h4>
+            <a
+              href="https://github.com/abelmore33/web-sprint-challenge-build-a-web-api"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img className="project-img" src={project1} alt="" />
+            </a>
+            <p>
+              In this project, I developed a CRUD API for managing projects and
+              actions using Node.js and Express. Key tasks included:
+            </p>
+
+            <ul>
+              <li>
+                Setting up and configuring the Node.js environment with NPM
+                scripts.
+              </li>
+              <li>Creating RESTful endpoints for projects and actions.</li>
+              <li>
+                Implementing middleware for logging, validation, and error
+                handling.
+              </li>
+              <li>
+                Designing database schemas and using SQLite for data
+                persistence.
+              </li>
+              <li>
+                Writing automated tests to ensure API functionality and
+                reliability.
+              </li>
+            </ul>
+            <p>This project showcases my skills in:</p>
+            <ul className="skills">
+              <li>
+                <img
+                  className="skills-img"
+                  src="https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png"
+                  alt="nodejs"
+                />
+              </li>
+              <li>
+                <img
+                  className="skills-img"
+                  src="https://cdn.icon-icons.com/icons2/2699/PNG/512/expressjs_logo_icon_169185.png"
+                  alt="express"
+                />
+              </li>
+              <li>
+                <img
+                  className="skills-img"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/SQLite370.svg/2560px-SQLite370.svg.png"
+                  alt=""
+                />
+              </li>
+            </ul>
+          </div>
+
+          <div className="project">
+            <h4>React Router Movies</h4>{" "}
+            <a
+              href="https://github.com/abelmore33/React-Router-Movies"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img className="project-img" src={project2} alt="" />
+            </a>
+            <p>
+              In this project, I built a movie application using React Router to
+              demonstrate client-side routing. Key tasks included:
+            </p>
+            <p>
+              In this project, I built a movie application using React Router to
+              demonstrate client-side routing. Key tasks included:
+            </p>
+            <ul>
+              <li>Setting up routes with React Router for different views.</li>
+              <li>
+                Implementing navigation between a list of movies and detailed
+                views of individual movies.
+              </li>
+              <li>
+                Utilizing URL parameters to dynamically display movie details.
+              </li>
+              <li>
+                Refactoring code to adhere to DRY (Don't Repeat Yourself)
+                principles.
+              </li>
+              <li>
+                Adding functionality to save favorite movies and navigate using
+                links.
+              </li>
+            </ul>
+            <p>This project showcases my skills in:</p>
+            <ul className="skills">
+              <li>
+                <img
+                  className="skills-img"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+                  alt="react"
+                />
+              </li>
+              <li>
+                <img
+                  className="skills-img"
+                  src="https://static-00.iconduck.com/assets.00/react-router-icon-2048x1116-jfeevj0l.png"
+                  alt="react-router"
+                />
+              </li>
+              <li>
+                <img
+                  className="skills-img"
+                  src="https://raw.githubusercontent.com/github/explore/6c6508f34230f0ac0d49e847a326429eefbfc030/topics/css/css.png"
+                  alt="css"
+                />
+              </li>
+            </ul>
+          </div>
+          <div className="project">
+            <h4>Web Sprint Challenge: Authentication and Testing</h4>
+            <a
+              href="https://github.com/abelmore33/web-sprint-challenge-authentication-and-testing"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={project3} alt="" className="project-img" />
+            </a>
+
+            <p>
+              In this project, I created an authentication system and
+              implemented testing for a Node.js application. Key tasks included:
+            </p>
+            <ul>
+              <li>
+                Setting up and configuring the Node.js environment with NPM
+                scripts.
+              </li>
+              <li>Creating RESTful endpoints for projects and actions.</li>
+              <li>
+                Implementing middleware for logging, validation, and error
+                handling.
+              </li>
+              <li>
+                Designing database schemas and using SQLite for data
+                persistence.
+              </li>
+              <li>
+                Writing automated tests to ensure API functionality and
+                reliability.
+              </li>
+            </ul>
+            <p>This project showcases my skills in:</p>
+            <ul className="skills">
+              <li>
+                <img
+                  className="skills-img"
+                  src="https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png"
+                  alt="nodejs"
+                />
+              </li>
+              <li>
+                <img
+                  src="https://e7.pngegg.com/pngimages/661/372/png-clipart-knex-js-full-logo-tech-companies.png"
+                  alt="knex"
+                  className="skills-img"
+                />
+              </li>
+              <li>
+                <img
+                  className="skills-img"
+                  src="https://cdn.icon-icons.com/icons2/2699/PNG/512/expressjs_logo_icon_169185.png"
+                  alt="express"
+                />
+              </li>
+            </ul>
+          </div>
+          <div className="project">
+            <h4>Project 4</h4>
+            <p>Description</p>
+          </div>
         </div>
       </div>
     </div>
