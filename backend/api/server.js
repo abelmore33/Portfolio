218eed4c788fcf40826da826e8da03cba0a7190f
belmoreaaron33 <belmoreaaron9@gmail.com>
2024-06-26 18:27:48 -0400

    linked fronend to backend

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const server = express();
+const cors = require("cors");
+
 const usersRouter = require("./users/users-router");
 
+server.use(cors());
 server.use(express.json());
 
 server.use("/api/users", usersRouter);
